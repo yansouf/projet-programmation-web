9299826016342186dc70ccbbb740fee04db70116
Haytham <fdlhaytham@gmail.com>
2018-04-17 20:12:24 +0100

    Ajout et changement des unites

--- slim/index.php
+++ slim/index.php
@@ -3,27 +3,30 @@ require 'vendor/autoload.php';
 $app = new \Slim\App();
 $app->get("/units/{name}",function($request, $response, $args){
 	$Poids = array(
-	'Kilograms' => 1,
-	 "Pounds"=>2.20462,
-	 'milligrams' => 1000000,
-	 'Centigrams' => 100000,
-	 'Dicigrams' => 10000,
-	 'Grams' => 1000,
-	 'Decagrams' => 100,
-	 'Hectograms' => 10,
-	 'Tons' => 0.001,
-	 'Ounces' =>35.27396);	
+	'Kilogramme' => 1,
+	'Hectogramme' => 10,
+	'Decagramme' => 100,
+	'Gramme' => 1000,
+	'Dicigramme' => 10000,
+	'Centigramme' => 100000,
+	'Pound'=>2.20462,
+	'Milligramme' => 1000000,
+	'Ton' => 0.001,
+	'Ounce' =>35.27396);
 	$Longeurs = array(
-	 "Meters"=>1,
-	 "Nanometer"=>1000000000,
-	 "Microns"=>1000000,
-	 "Millimeters"=>1000,
-	 "Centimeter"=>100,
-	 "Kilometers"=>0.001,
-	 "Inches"=>39.37008,
-	 "Feet"=>3.28084,
-	 "Yards"=>1.09313,
-	 "Miles"=>0.000621
+	"Kilometer"=>0.001,
+	"Hectometre"=>0.01,
+	"Decametre"=>0.1,
+	"Metre"=>1,
+	"Decimetre"=>10,	 
+	"Centimetre"=>100,
+	"Millimetre"=>1000,
+	"Micrometre"=>1000000,
+	"Nanometre"=>1000000000,
+	"Inche"=>39.37008,
+	"Feet"=>3.28084,
+	"Yard"=>1.09313,
+	"Mile"=>0.000621
 );
 	$unites=array("Poids"=>$Poids,"Longeurs"=>$Longeurs);
  	if (isset($unites[$args['name']])) {
